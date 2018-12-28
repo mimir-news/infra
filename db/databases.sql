fa1da4ca51f910a81871841a60b841f7a0799e35
Simon Lindgren <simon.g.lindgren@gmail.com>
2018-12-28 16:59:16 +0100

    MQ queues update + DB definitions update2

--- db/databases.sql
+++ db/databases.sql
@@ -1,6 +1,10 @@
-CREATE DATABASE streamlistner;
-CREATE ROLE streamlistner WITH LOGIN PASSWORD 'password';
-GRANT ALL PRIVILEGES ON DATABASE streamlistner TO streamlistner;
+CREATE DATABASE streamlistener;
+CREATE ROLE streamlistener WITH LOGIN PASSWORD 'password';
+GRANT ALL PRIVILEGES ON DATABASE streamlistener TO streamlistener;
+
+CREATE ROLE stocksearch WITH LOGIN PASSWORD 'password';
+GRANT CONNECT ON DATABASE streamlistner TO stocksearch;
+GRANT USAGE ON SCHEMA public TO stocksearch;
 
 CREATE DATABASE spamfilter;
 CREATE ROLE spamfilter WITH LOGIN PASSWORD 'password';
@@ -9,6 +13,10 @@ GRANT ALL PRIVILEGES ON DATABASE spamfilter TO spamfilter;
 CREATE DATABASE newsranker;
 CREATE ROLE newsranker WITH LOGIN PASSWORD 'password';
 GRANT ALL PRIVILEGES ON DATABASE newsranker TO newsranker;
+
+CREATE ROLE newssearch WITH LOGIN PASSWORD 'password';
+GRANT CONNECT ON DATABASE newsranker TO newssearch;
+GRANT USAGE ON SCHEMA public TO newssearch;
 
 CREATE DATABASE directory;
 CREATE ROLE directory WITH LOGIN PASSWORD 'password';
