1ee0e6c624be4a22ef732ef8a6e01363aed1a4af
Simon Lindgren <simon.g.lindgren@gmail.com>
2019-01-08 12:59:20 +0100

    Changed connection pooler config

--- db/databases.sql
+++ db/databases.sql
@@ -1,23 +1,23 @@
 CREATE DATABASE streamlistener;
-CREATE ROLE streamlistener WITH LOGIN PASSWORD 'password';
+CREATE ROLE streamlistener WITH LOGIN PASSWORD 'streamlistener';
 GRANT ALL PRIVILEGES ON DATABASE streamlistener TO streamlistener;
 
-CREATE ROLE stocksearch WITH LOGIN PASSWORD 'password';
+CREATE ROLE stocksearch WITH LOGIN PASSWORD 'stocksearch';
 GRANT CONNECT ON DATABASE streamlistener TO stocksearch;
 GRANT USAGE ON SCHEMA public TO stocksearch;
 
 CREATE DATABASE spamfilter;
-CREATE ROLE spamfilter WITH LOGIN PASSWORD 'password';
+CREATE ROLE spamfilter WITH LOGIN PASSWORD 'spamfilter';
 GRANT ALL PRIVILEGES ON DATABASE spamfilter TO spamfilter;
 
 CREATE DATABASE newsranker;
-CREATE ROLE newsranker WITH LOGIN PASSWORD 'password';
+CREATE ROLE newsranker WITH LOGIN PASSWORD 'newsranker';
 GRANT ALL PRIVILEGES ON DATABASE newsranker TO newsranker;
 
-CREATE ROLE newssearch WITH LOGIN PASSWORD 'password';
+CREATE ROLE newssearch WITH LOGIN PASSWORD 'newssearch';
 GRANT CONNECT ON DATABASE newsranker TO newssearch;
 GRANT USAGE ON SCHEMA public TO newssearch;
 
 CREATE DATABASE directory;
-CREATE ROLE directory WITH LOGIN PASSWORD 'password';
+CREATE ROLE directory WITH LOGIN PASSWORD 'directory';
 GRANT ALL PRIVILEGES ON DATABASE directory TO directory;
